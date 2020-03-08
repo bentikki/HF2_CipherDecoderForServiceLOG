fd0fbd679f9cbc3b1ef56ba8f97cc9ed1b3536c8
Benjamin Tikki Olsen <benjamin.tikki@gmail.com>
2020-03-08 16:53:18 +0100

    Final

---
+++ CipherDecoder/CipherDecoder/DecodeFile.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CipherDecoder
+{
+    public class DecodeFile
+    {
+        public static string Path { get; } = AppDomain.CurrentDomain.BaseDirectory + @"\LogDecoded\";
+        public static string FilePath { get; } = Path + "Logfile.txt";
+
+        public void TranslateFile(string path)
+        {
+            string[] lines = File.ReadAllLines(path);
+            StreamWriter sw = null;
+
+            try
+            {
+                sw = new StreamWriter(FilePath);
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    string decryptedString = Cipher.Decrypt(lines[i]);
+                    Console.WriteLine(decryptedString);
+                    sw.WriteLine(decryptedString);
+                }
+                sw.Flush();
+                sw.Close();
+
+                Console.WriteLine("Created file at: " + FilePath);
+            }
+            catch
+            {
+
+                throw;
+            }
+
+        }
+    }
+}
---
+++ CipherDecoder/CipherDecoder/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CipherDecoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DecodeFile decode = new DecodeFile();
+
+            Console.WriteLine("Decode file name:");
+            string fileName = Console.ReadLine();
+
+            decode.TranslateFile(fileName);
+
+        }
+    }
+}
